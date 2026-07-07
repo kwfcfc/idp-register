0eae89e22819975a43a49e4f236ee2623381348e
gobro <gobro@noreply.localhost>
2026-07-06 21:51:35 -0400

    dev(flake): add crow-ci input for devshell and crow-cli tool

--- flake.nix
+++ flake.nix
@@ -3,13 +3,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     flake-utils.url = "github:numtide/flake-utils";
+    crow-ci.url = "https://codefloe.com/crowci/crowci-flake/archive/main.tar.gz";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, crow-ci, ... }:
     # with flake-utils.lib; eachSystem allSystems (system:
     with flake-utils.lib; eachSystem ["x86_64-linux" "aarch64-darwin"] (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ crow-ci.overlays.default ];
+        };
       in {
         devShells.default = pkgs.mkShell {
           # TEMP WORKAROUND (do not commit): nix's nodejs_24 24.15.0 on
@@ -21,7 +25,7 @@
           buildInputs = [
             pkgs.go
             pkgs.gopls
-            pkgs.typescript-language-server
+            pkgs.crow-cli
           ]
             ++ pkgs.lib.optionals (system == "aarch64-darwin") [ pkgs.pngpaste ]
             ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ pkgs.wl-clipboard ];
